cb04e7e09bd4a373473520c7e5ac681627e2e588
N1GHTMARE <derebanoski@aap-ndt.com>
2021-08-09 12:26:40 +0200

    Intial setup with PDF tron

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from "react";
+import { Core } from "@pdftron/webviewer";
+import { saveAs } from "file-saver";
 
 function App() {
+  const saveBufferAsPDFDoc = (buf, name) => {
+    saveBuffer(buf, name, "application/pdf");
+  };
+
+  const saveBuffer = (buf, name, mimetype) => {
+    const blob = new Blob([buf], {
+      type: mimetype,
+    });
+    saveAs(blob, name);
+  };
+
   const generatePdfDocument = () => {
-    return alert(`Generate PDF out of the docx document "PDF DOCUMENT" `);
+    const PDFNet = Core.PDFNet;
+    const convertOfficeToPDF = (inputUrl, outputName, l) =>
+      Core.office2PDFBuffer(inputUrl, { l }).then((buffer) => {
+        saveBufferAsPDFDoc(buffer, outputName);
+        console.log(`Finished downloading ${outputName}`);
+      });
+
+    const runOfficeToPDF = (inputUrl, type) => {
+      PDFNet.initialize()
+        .then(() => convertOfficeToPDF(inputUrl, `converted_${type}.pdf`))
+        .then(() => {
+          console.log("Test Complete!");
+        })
+        .catch((err) => {
+          console.log("An error was encountered! :(", err);
+        });
+    };
+
+    runOfficeToPDF("https://docxtemplater.com/tag-example.docx", "DOCX");
   };
 
   return (
