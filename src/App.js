fd8d4475ce3a9e0c8dc0ca7d6b467da3eab85855
N1GHTMARE <derebanoski@aap-ndt.com>
2021-08-09 11:50:04 +0200

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const generatePdfDocument = () => {
+    return alert(`Generate PDF out of the docx document "PDF DOCUMENT" `);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Generate PDF DOCUMENT WITH THE example you have provided for us</h1>
+      <button onClick={generatePdfDocument}>Generate PDF</button>
+    </>
   );
 }
 
